60bb1c633bed1258a279641c9d0b9d7f86a1d202
Andrew <andrew@Alphas-Air.home.local>
2016-11-07 18:45:38 -0800

    removed unused script
    removed the zoe.js script that wasn’t being used

--- js/zoe.js
+++
@@ -1,8 +0,0 @@
-// event listener for the 'clear' button
-document.addEventListener('DOMContentLoaded', function() {
-  document.getElementById("clear").addEventListener("click", reload);
-});
-
-function reload() {
-  try{for(var lastpass_iter=0; lastpass_iter < document.forms.length; lastpass_iter++){ var lastpass_f = document.forms[lastpass_iter]; if(typeof(lastpass_f.lpsubmitorig2)=="undefined"){ lastpass_f.lpsubmitorig2 = lastpass_f.submit; if (typeof(lastpass_f.lpsubmitorig2)=='object'){ continue;}lastpass_f.submit = function(){ var form=this; var customEvent = document.createEvent("Event"); customEvent.initEvent("lpCustomEvent", true, true); var d = document.getElementById("hiddenlpsubmitdiv"); if (d) {for(var i = 0; i < document.forms.length; i++){ if(document.forms[i]==form){ if (typeof(d.innerText) != 'undefined') { d.innerText=i.toString(); } else { d.textContent=i.toString(); } } } d.dispatchEvent(customEvent); }form.lpsubmitorig2(); } } }}catch(e){}
-}
