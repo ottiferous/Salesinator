9b6f0e32d130b9e825400ab9232f629ce55ec849
Andrew <andrew@Alphas-MacBook-Air.local>
2016-11-11 12:51:38 -0800

    added MOTD functionality into page
    when the page is loaded, or clear is pressed it will now generate a
    MOTD style message under the heading.

--- js/generate.js
+++ js/generate.js
@@ -1,7 +1,9 @@
 // event listener for the 'submit' button
 document.addEventListener('DOMContentLoaded', function(evt) {
   document.getElementById("submit").addEventListener("click", getSalesinatorOptions);
+  motd();
   evt.preventDefault();
+
 });
 
 /*
@@ -30,5 +32,35 @@ function getSalesinatorOptions() {
   }
   //console.log("FINALHTML: " + finalHTML);
   document.getElementById('foobar').innerHTML += finalHTML;
+  return false;
+}
+
+function motd() {
+  // generate a somewhat sensical message to show below the title
+  // There is no real reason for this other than it is fun.
+  // Isn't that reason enough?
+
+  let wordlist = [
+    "Generating documentation since 2016!",
+    "How do you turn this thing on?",
+    "Help! I'm trapped in a sandbox!",
+    "Would I lie? Can I lie?",
+    "Because we can't say RTFM",
+    "Save the whales!",
+    "So Long! And thanks for all the fish.",
+    "If at first you don't succeed, skydiving is not for you.",
+    "Duct tape is like THE FORCE; it has a light side & a dark side, and it holds the universe together.",
+    "Anything is possible if you don't know what you're talking about.",
+    "File that under 'Never'.",
+    "Oh hello there. I didn't see you. Probably from a lack of eyes.",
+    "<insert funny quote here>",
+    "Well I think you're stunning, brave, and beautiful.",
+    "Salesinator was recently updated. It now nows how to experience love!",
+    "I saw a turtle!",
+    "SELECT ALL THE THINGS!",
+    "It sounds like English, but I can't understand a word you're saying.",
+    "Life's true gift is the capacity to enjoy enjoyment."
+  ];
+  document.getElementById('motd').innerHTML += wordlist[Math.floor(Math.random()*wordlist.length)];
   return false;
 }
