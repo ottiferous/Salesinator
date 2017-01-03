8bae747166bf6477daf876210a8deb1cc53c177b
Andrew <github@andrewmarrone.com>
2017-01-03 15:10:03 -0500

    option tags are built through javascript
    Dynamically builds <option> sections

--- js/generate.js
+++ js/generate.js
@@ -2,8 +2,23 @@
 document.addEventListener('DOMContentLoaded', function(evt) {
   document.getElementById("submit").addEventListener("click", getSalesinatorOptions);
   motd();
+
+  /* dynamically geneate the drop down menus */
   buildHowTo();
-  buildIntro(generateIntro());
+  buildOptionList(generateIntro(), 'introduction');
+  buildOptionList(generatePlatformFeatures(), 'platform_features');
+  buildOptionList(generateEnterpriseFeatures(), 'enterprise_features');
+  buildOptionList(generateAdministration(), 'administration');
+
+  buildOptionList(generateVPN(), 'vpn');
+  buildOptionList(generateMicrosoft(), 'microsoft');
+  buildOptionList(generateWeb(), 'web');
+  buildOptionList(generateOther(), 'other');
+
+  buildOptionList(generateCloud(), 'cloud');
+  buildOptionList(generateAPI(), 'api');
+  buildOptionList(generateReference(), 'reference');
+
   evt.preventDefault();
   document.getElementById("")
 
@@ -97,14 +112,14 @@ function buildOptionHTML(key, value) {
 }
 
 /* builds the Intro Section based on a passed Dictionary */
-function buildIntro(list) {
+function buildOptionList(list, section) {
 
   /* how many elements to create */
   let maxCount = 3;
   var finalHTML = "";
 
   for ( x = 0; x < maxCount; x++ ) {
-    finalHTML += "<select id=\"Introduction" + x + "\">";
+    finalHTML += "<select id=\"" + section + x + "\">";
     finalHTML += "<option label=\" \"></option>\n";
     for (var key in list) {
       if(list.hasOwnProperty(key)) {
@@ -114,9 +129,13 @@ function buildIntro(list) {
     }
     finalHTML += "</select>"
   }
-  document.getElementById('introduction').innerHTML += finalHTML;
+  document.getElementById(section).innerHTML += finalHTML;
   return false;
 }
+
+/*
+  Helper functions to build the <option> list through a dictionary
+*/
 function generateIntro() {
   return {
     "guide" : "Guide",
@@ -125,5 +144,175 @@ function generateIntro() {
     "creating_applications" : "Creating Applications",
     "enrolling_users" : "Enrolling Users",
     "deploying_a_proof_of_concept" : "Deploying a Proof of Concept"
+  };
+}
+function generatePlatformFeatures() {
+  return {
+    "platform_overview" : "Platform Overview",
+    "policy" : "Policy & Control",
+    "insights" : "Device Insight",
+    "endpoints" : "Endpoints",
+    "dag" : "Duo Access Gateway"
+  };
+}
+function generateEnterpriseFeatures() {
+  return {
+    "admin-roles" : "Administration Roles",
+    "self-service-portal" : "Self Service Portal",
+    "syncing_users_from_active_directory" : "Directory Sync",
+    "trusted_devices" : "Trusted Devices & Networks",
+    "device-management" : "Device Management Portal"
+  };
+}
+function generateAdministration() {
+  return {
+    "administration-settings" : "Changing Settings",
+    "administration-users" : "Managing Users",
+    "administration-devices" : "Managing Devices",
+    "telephony_credits" : "Telephony Credits",
+    "accessibility" : "Accessibility",
+    "labs-features" : "Labs Features"
+  };
+}
+function generateVPN() {
+  return {
+    "juniper" : "juniper",
+    "pulseconnect" : "Pulse Secure",
+    "cisco" : "Cisco",
+    "citrix" : "Citrix NetScaler",
+    "paloalto" : "Palo Alto",
+    "f5bigip" : "F5",
+    "fortinet" : "Fortinet",
+    "barracuda" : "Barracuda",
+    "array-ag" : "Array Networks",
+    "sonicwallsra" : "Sonicwall SRA",
+    "openvpn" : "OpenVPN",
+    "openvpnas" : "OpenVPN AS",
+    "netmotion" : "NetMotion Wireless",
+    "checkpoint" : "Checkpoint",
+    "radius" : "RADIUS",
+    "authproxy" : "Auth Proxy"
+  };
+}
+function generateMicrosoft() {
+  return {
+    "owa" : "Outlook Web App",
+    "o365" : "Office 365",
+    "rdp" : "Windows Local Login & RDP",
+    "adfs-30" : "Active Directory Federation Services",
+    "rds" : "Remote Desktop Services",
+    "tmg" : "Threat Management Gateway",
+    "uag" : "Unified Access Gateway",
+    "rras" : "RRAS"
+  };
+}
+function generateWeb() {
+  return {
+    "duoweb" : "Duo Web SDK",
+    "wordpress" : "WordPress",
+    "confluence" : "Confluence (local)",
+    "jira" : "JIRA (local)",
+    "drupal" : "Drupal",
+    "splunk" : "Splunk",
+    "shibboleth" : "Shibboleth",
+    "oam" : "Oracle Access Manager",
+    "lastpass" : "LastPass",
+    "1password" : "1Password"
+  };
+}
+function generateOther() {
+  return {
+    "epic" : "Epic",
+    "awsworkspaces" : "Amazon WorkSpaces",
+    "peoplesoft" : "Oracle PeopleSoft",
+    "juniper-uac" : "Juniper Unified Access Control",
+    "vmwareview" : "VMWare Horizon View",
+    "bomgar" : "Bomgar",
+    "thycotic" : "Thycotic Secret Server",
+    "ldap" : "LDAP",
+    "radius" : "RADIUS",
+    "duounix" : "Unix & SSH (PAM)"
+  };
+}
+function generateCloud() {
+  return {
+    "asana" : "Asana",
+    "adobe-documentcloud" : "Adobe Document Cloud",
+    "aws" : "Amazon Web Services",
+    "bamboohr" : "BambooHR",
+    "bluejeans" : "BlueJeans",
+    "bomgar-soo" : "Bomgar SSO",
+    "bonusly" : "Bonusly",
+    "box" : "Box",
+    "canvas" : "Canvas",
+    "clarizen" : "Clarizen",
+    "cloudlock" : "CloudLock",
+    "confluencesso" : "Confluence SSO",
+    "crashplan" : "CrashPlan",
+    "datadog" : "Datadog",
+    "desk" : "Desk",
+    "docusign" : "DocuSign",
+    "dropbox" : "Dropbox",
+    "egnyte" : "Egnyte",
+    "evernote" : "Evernote",
+    "expensify" : "Expensify",
+    "freshdesk" : "Freshdesk",
+    "github-enterprise" : "Github",
+    "gotomeeting" : "GoToMeeting",
+    "greenhouse" : "Greenhouse",
+    "gapps" : "G Suite (Google Apps)",
+    "hackerone" : "HackerOne",
+    "heroku" : "Heroku",
+    "intacct" : "Intacct",
+    "jamf-jss" : "JAMF Software",
+    "jitbit" : "JitBit",
+    "jirasso" : "Jira SSO",
+    "looker" : "Looker",
+    "marketo" : "Marketo",
+    "meraki" : "Meraki",
+    "namely" : "Namely",
+    "new-relic" : "New Relic",
+    "o365" : "Office 365",
+    "opendns" : "OpenDNS",
+    "pagerduty" : "Pagerduty",
+    "remedyforce" : "Remedyforce",
+    "ringcentral" : "RingCentral",
+    "robin" : "Robin",
+    "salesforce" : "Salesforce",
+    "samanage" : "Samanage",
+    "saucelabs" : "SauceLabs",
+    "sharefile" : "ShareFile",
+    "signalsciences" : "Signal Sciences",
+    "slack" : "Slack",
+    "smartsheet" : "Smartsheet",
+    "statuspageio" : "StatusPage.io",
+    "sugarcrm" : "SugarCRM",
+    "sumologic" : "Sumologic",
+    "syncplicity" : "Syncplicity",
+    "udemy" : "Udemy",
+    "Uservoice" : "User Voice",
+    "webex" : "WebEx",
+    "facebookatwork" : "Facebook at Work",
+    "zendesk" : "Zendesk",
+    "zoom" : "Zoom",
+    "onelogin" : "One Login",
+    "okta" : "Okta",
+    "pingfederate" : "Ping Federate"
+  };
+}
+function generateAPI() {
+  return {
+    "duoweb" : "DuoWeb SDK",
+    "authapi" : "Auth API",
+    "adminapi" : "Administration API"
+  };
+}
+function generateReference() {
+  return {
+    "authrpoxy" : "Authproxy",
+    "duo_help_desk_guide" : "Duo Help Desk Guide",
+    "guide_to_business_continuity_preparedness" : "Business Continuity Preparedness",
+    "promoting_push" : "Promoting Push Guide",
+    "checksums" : "Checksums"
   };
 }
